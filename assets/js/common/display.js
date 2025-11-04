2dc78473ee8ce6c7569383be105566d6ffd9cbe2
tomoya <nameneko1123@gmai.com>
2025-11-05 00:12:26 +0900

    制御修正

--- assets/js/common/display.js
+++ assets/js/common/display.js
@@ -1,15 +1,5 @@
 /*pc画面、スマホ画面の表示制御*/
 
-function switchDisplay(mode) {
-  const body = document.body;
-  body.classList.remove('pc-view', 'sp-view');
-  if (mode === 'pc') {
-    body.classList.add('pc-view');
-  } else if (mode === 'sp') {
-    body.classList.add('sp-view');
-  }
-}
-
 window.onload = function () {
   const displayCookie = document.cookie
     .split('; ')
